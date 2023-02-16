af2ef55fd5584518b2c942b36c2ab56bb6a201ac
Automation Tester <gyansahoo666@gmail.com>
2023-02-16 05:10:53 -0800

    Files added

---
+++ target/gatling/computerdatabase-20230216131009900/js/all_sessions.js
@@ -0,0 +1,11 @@
+allUsersData = {
+    
+color: '#FF9D00',
+name: 'Active Users',
+data: [
+  [1676553012000,1],[1676553013000,1]
+],
+tooltip: { yDecimals: 0, ySuffix: '', valueDecimals: 0 }
+    , zIndex: 20
+    , yAxis: 1
+};
